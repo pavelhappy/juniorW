56a38602d18cd241828d3ffb5fb6028e45f7606d
Pavel Pogorelov <stosman@yandex.ru>
2015-10-15 14:32:39 +1000

    small optimization

--- src/Task1/UniqueArray.java
+++ src/Task1/UniqueArray.java
@@ -10,19 +10,16 @@ public class UniqueArray {
         for (int i = 0; i < integers.length; i++) {
             tempList.add(integers[i]);
         }
-        int size = tempList.size();
-        int duplicates = 0;
 
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = i + 1; j < size; j++) {
+        for (int i = 0; i < tempList.size() - 1; i++) {
+            for (int j = i + 1; j < tempList.size() - 1; j++) {
                 if (!tempList.get(j).equals(tempList.get(i)))
                     continue;
-                duplicates++;
                 tempList.remove(j);
                 j--;
-                size--;
             }
         }
+
         int[] result = new int[tempList.size()];
         for (int i = 0; i < result.length; i++) {
             result[i]= tempList.get(i);
